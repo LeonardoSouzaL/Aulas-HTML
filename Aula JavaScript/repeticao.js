88c425b026d257dd75282251b37c39f553e8a1dd
Leonardo Souza Leite <leonardoleoleite21@gmail.com>
2026-04-24 21:50:19 -0300

    .

--- Aula JavaScript/repeticao.js
+++ Aula JavaScript/repeticao.js
@@ -74,7 +74,7 @@ let y = 10;
 do {
     console.log("Isso vai aparecer uma vez")
 } while (y < 5);
-*/
+
 // Menu Interativo
 let opcao;
 let saldo = 1000;
@@ -120,7 +120,7 @@ do {
     }
 } while (opcao !== "0");
 
-/*
+
 // Jogo: Adivinhe o número
 const numeroSecreto = Math.floor(Math.random() * 10) + 1;
 // Math.random 0.0 0.9999999999...
@@ -147,3 +147,128 @@ do {
     }
 } while (palpite !== numeroSecreto);
 */
+
+//const { captureOwnerStack } = require("react");
+
+// For
+/* 
+console.log("Contando de 1 a 10:\n")
+for (let i = 1; i <= 10; i++) {
+    console.log(`Número: ${i}`)
+}
+
+
+// Tabuada 
+const numero = Number(prompt("Tabuada de qual número: "))
+if (Number.isNaN(numero)) {
+    console.log("Número inválido!")
+} else {
+    console.log(`\n Tabuada do ${numero}\n`);
+
+    for (let i = 1; i <= 10; i++){
+        const resultado = numero * i;
+        console.log(`${numero} X ${i} = ${resultado}`)
+    }
+}
+
+
+// Números pares (0 a 20)
+console.log("Mostrando os números pares de 0 a 20\n");
+for(let i = 0; i <= 20; i+= 2){
+    console.log(`Número par: ${i}`)
+}
+
+
+// Contagem regressiva
+console.log("Iniciando contagem regressiva!! \n");
+for(let i = 100; i >= 0; i--){
+    console.log(i)
+}
+
+
+const altura = 5;
+for (let linha = 1; linha <= altura; linha++){
+    let asteriscos = "";
+
+    for (let i = 1; i <= linha; i++){
+        asteriscos += "* "
+    }
+    console.log(asteriscos);
+}
+
+
+// Array (Lista)
+const frutas = ["Maçã", "Banana", "Laranja", "Uva"]
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+console.log(frutas[3]);
+
+console.log(frutas.length);
+
+
+//for of
+for (const item of frutas) {
+    console.log(`- ${frutas}`)
+}
+
+
+// Soma de todos os número do arry
+const numeros = [10, 25, 8, 42, 15, 7];
+let soma = 0;
+for (const item of numeros ) {
+    soma += item
+}
+console.log(`A soma de todos os números da sua lista é de: ${soma}`)
+
+
+// Objetos
+const pessoa = {
+    nome: "Leonardo",
+    idade: 25,
+    cidade: "Suzano",
+    profissao: "Faz tudo"
+}
+
+console.log(pessoa.nome)
+console.log(pessoa["idade"])
+
+//for in
+for (const chave in objeto){
+
+}
+
+
+const aluno = {
+    nome: "Maria Silva",
+    matricula: 20223401,
+    curso: "Desenvolvedor Web",
+    nota: 9.5
+};
+
+console.log(`Dados do Aluno: \n`);
+for (const chave in aluno) {
+    const valor = aluno[chave]
+    console.log(`${chave}: ${valor}`)
+}
+*/
+
+// Estoque de produtos
+const estoque = {
+    "notebook": 15,
+    "Mouse": 50,
+    "Teclado": 30,
+    "Monitor": 8,
+    "Webcam": 22
+}
+
+console.log("Quantidade em Estoque: \n");
+for(const produto in estoque){
+    const quantidade = estoque [produto];
+    
+    if (quantidade < 10) {
+        console.log(`Baixo estoque, providencie a compra de mais, ${produto}: ${quantidade} unidades`);
+    } else {
+        console.log(`${produto}: ${quantidade}`)
+    }
+}
